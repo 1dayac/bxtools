d6294835adee8124143fd5c9e6b30c9d90c5f4e1
Dima <dima@mac178124.med.cornell.edu>
2018-02-08 15:11:50 -0500

    Temp

--- src/bxfilter.cpp
+++ src/bxfilter.cpp
@@ -105,7 +105,7 @@ static bool CheckConditions(const std::vector<SeqLib::BamRecord> &records) {
         return true;
     } else {
         for (const auto &record : records) {
-            if (record.MapQuality() < opt::mapping_quality) {
+            if (record.NumMatchBases()/(double)record.Length() < 0.1) {
                 std::cerr << "Filtered: bad mapping quality" << std::endl;
                 return true;
             }
